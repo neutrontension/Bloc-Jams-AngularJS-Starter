fce1ade1c20da7b5b6a9391e6f01388098ae64aa
Roxie <Roxie@gigabyte.home>
2017-11-12 21:26:58 -0800

    checkpoint 3 routing and states completed

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,30 @@
-angular.module('blocJams', []);
+(function() {
+    function config($stateProvider, $locationProvider) {
+      /* $stateProvider, a component of UI-router, will determine
+      the number of properties for a state.
+          states in our bloc jams app: name, URL route, controller,
+          and template.
+
+      $locationProvider, part of Angular's core, configures an
+      application's paths. */
+      $locationProvider
+          .html5Mode({
+              enabled: true,
+              requireBase: false
+          });
+
+      $stateProvider
+          .state('landing', {
+              url: '/',
+              templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            });       
+    }
+
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);
+})();
