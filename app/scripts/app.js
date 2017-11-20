5b9360a8769ded2d9cf20ba6443c709a99dd1ff8
Roxie <Roxie@gigabyte.home>
2017-11-19 21:44:03 -0800

    checkpoint 5 controllers complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,7 @@
       $stateProvider
           .state('landing', {
               url: '/',
+              controller: 'LandingCtrl as landing',
               templateUrl: '/templates/landing.html'
             })
             .state('album', {
@@ -24,6 +25,7 @@
             })
             .state('collection', {
                 url: '/collection',
+                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
             });
     }
